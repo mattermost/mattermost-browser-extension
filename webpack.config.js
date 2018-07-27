44926690c19e67edb45c13fa4c43f53076c419f2
JoramWilander <jwawilander@gmail.com>
2018-07-27 12:58:23 -0400

    Update webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,16 +2,38 @@
     ./webpack.config.js
 */
 const path = require('path');
+
+const STANDARD_EXCLUDE = [
+  path.join(__dirname, 'node_modules'),
+  path.join(__dirname, 'non_npm_dependencies'),
+];
+
 module.exports = {
   entry: './src/app/index.js',
   output: {
     path: path.resolve('dist'),
-    filename: 'bundle.js'
+    filename: 'bundle.js',
   },
   module: {
-    loaders: [
-      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
-      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
-    ]
-  }
+    rules: [
+      {
+          test: /\.(js|jsx)?$/,
+          exclude: STANDARD_EXCLUDE,
+          use: [
+              {
+                  loader: 'babel-loader',
+                  options: {
+                      presets: [
+                          'react',
+                          ['es2015', {modules: false}],
+                          'stage-0',
+                      ],
+                      plugins: ['transform-runtime'],
+                      cacheDirectory: true,
+                  },
+              },
+          ],
+      },
+    ],
+  },
 }
